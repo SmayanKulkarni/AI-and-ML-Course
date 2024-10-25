4abf9ecd72b23c6262d763add0b96e0c63afabb8
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-10-25 21:34:03 +0530

    basics of flask

--- Backend/Flask/getpost.py
+++ Backend/Flask/getpost.py
@@ -1,14 +1,20 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to my Flask app!"
+    return render_template('index.html')
 
 @app.route('/success/<int:score>')
 def success(score):
-    return "You have passed with a score of: " + str(score)
+    res = ""
+    if score < 50 :
+        res = 'fail'
+    else:
+        res = 'success'
+    exp = {'score': score , 'res' : res}
+    return render_template('result.html', result = exp)
 
 @app.route('/fail/<int:score>')
 def fail(score):
@@ -27,11 +33,18 @@ def results(marks):
 def submit():
     total_score = 0
     if request.method == 'POST':
-        datepreprocess = float(request.form['subject1'])
+        datapreprocess = float(request.form['subject1'])
         DE =float(request.form['subject2'])
         ML = float(request.form['subject3'])
         DL = float(request.form['subject4'])
-        total_score = 
+        total_score = datapreprocess + DE+ ML + DL 
+        res = ""
+        if total_score < 50:
+            res = 'fail'
+        else:
+            res = 'success'
+        
+        return redirect(url_for('success', score = total_score))
     
         
 if __name__ == '__main__':
